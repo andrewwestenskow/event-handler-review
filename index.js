7b26cebfb57acc8cf731039f598fdb269901385b
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-01-22 10:49:11 -0700

    Added snickers example

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 console.log(`GOOD MORNING`)
 
-const funDip = document.querySelector('.fun-dip')
+const funDip = document.getElementById('fun-dip')
+
+//Change styles directly
 
 funDip.addEventListener('click', e => {
-  funDip.classList.toggle('class-4')
-  funDip.classList.toggle('blue')
-  funDip.classList.toggle('green')
+  funDip.style.height = '500px'
+  funDip.style.width = '500px'
+
+  if (funDip.style.backgroundColor === 'rgb(186, 218, 85)') {
+    funDip.style.backgroundColor = 'aqua'
+  } else {
+    funDip.style.backgroundColor = '#bada55'
+  }
 })
 
 funDip.addEventListener('mouseenter', e => {
@@ -15,3 +22,18 @@ funDip.addEventListener('mouseenter', e => {
 funDip.addEventListener('mouseleave', e => {
   funDip.style.backgroundColor = 'aqua'
 })
+
+//Swap class names
+
+const snickers = document.querySelector('.snickers')
+
+snickers.addEventListener('click', handleOpen)
+
+function handleOpen() {
+  snickers.classList.toggle('open')
+  if (snickers.innerText !== 'SATISFIED') {
+    snickers.innerText = 'SATISFIED'
+  } else {
+    snickers.innerText = `You're not you when you're hungry`
+  }
+}
